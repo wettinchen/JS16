8956797032a552fabbfdce1be5016b3cd8f74697
suncrownfisher <suncrownfisher@gmail.com>
2023-07-06 18:39:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## JavaScript Chapter 16
+It is my coding practice with the TUTORIAL of Dave Gray. 
+
+## Source
+### Dave Gray 的 JavaScript 課程
+https://youtube.com/playlist?list=PL0Zuz27SZ-6Oi6xNtL_fwCrwpuqylMsgT
+
+### Dave Gray 的 YouTube 頻道
+https://www.youtube.com/@DaveGrayTeachesCode
+
+## JavaScript Chapter 16
+   Quick Concept outline
+   中文摘要說明與重點提問
+
+###  1. Intro 
+        教學影片開頭和介紹
+
+###  2. Refactoring Code: Rock, Paper, Scissors
+        Comparing version 1 and version 2
+        (1)Adding a loop
+        (2)Adding an array
+        (3)Eliminating the reload
+
+###  3. Testing our code
+        Test version 2
+
+###  4. Code
+        Comparing version 2 and version 3
+        A functional approach
---
+++ js/main16v2.js
@@ -0,0 +1,40 @@
+// Rock, Paper, Scissors: Refactored with While Loop and an Array
+let playGame = confirm("Shall we play rock, paper, or scissors?");
+
+if (playGame) {
+    // play
+    while (playGame) {
+        const playerChoice = prompt("Please enter rock, paper, or scissors.");
+        if (playerChoice || playerChoice === "") {
+            const playerOne = playerChoice.trim().toLowerCase();
+            if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+                const computerChoice = Math.floor(Math.random() * 3);
+                const rpsArray = ["rock", "paper", "scissors"];
+                const computer = rpsArray[computerChoice];
+                
+                const result = playerOne === computer
+                    ? "Tie game!"
+                    : playerOne === "rock" && computer === "paper"
+                    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                    : playerOne === "paper" && computer === "scissors"
+                    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                    : playerOne === "scissors" && computer === "rock"
+                    ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                    : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+                alert(result);
+    
+                const playAgain = confirm("Play Again?");
+                if (!playAgain) alert("Ok, thanks for playing.");
+                continue;
+            } else {
+                alert("You did not enter rock, paper, or scissors.");
+                continue;
+            }
+        } else {
+            alert("I guess you changed your mind. Maybe next time.");
+            break;
+        }
+    }
+} else {
+    alert("Ok, maybe next time.");
+}
